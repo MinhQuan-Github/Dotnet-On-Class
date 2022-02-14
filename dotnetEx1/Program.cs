5ed7d93c8acfe9b8ec3553cff41e2123b96dac7e
MinhQuan-Github <minhquan.5322@aol.com>
2022-02-14 22:55:41 +0700

    create project 1

---
+++ dotnetEx1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DOTNET // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static void swap(ref int a , ref int b){
+            int t = a ;
+            a = b;
+            b = t;
+        }
+        static void Main(string[] args)
+        {
+            // int x = 100 ;
+            // object obj = new Object();
+            // obj = x;
+            // Console.Write(obj);
+            // object obj ;
+            // int x = 100;
+            // obj = x;
+            // int y = (int)obj;
+            // Console.Write(y);
+            int a = 100 , b = 2;
+            swap(ref a , ref b);
+            Console.Write("a = {0} , b = {1}" , a , b);
+        }
+    }
+}
