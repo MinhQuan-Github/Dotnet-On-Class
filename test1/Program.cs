1620ed5c02515d42cd732c97dc87a1789e962476
MinhQuan-Github <minhquan.5322@aol.com>
2022-03-01 15:59:41 +0700

    add test 1

---
+++ test1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tes1
+{
+    public class Program
+    {
+        static void Main(String [] args){
+            try {
+                f2();
+            }
+            catch (DivideByZeroException dex) 
+            {
+                Console.Write("Divide by zero exception : \n" + dex.StackTrace);
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine("General exception :" + ex.Message);
+            }
+            Console.ReadLine();
+        }
+        static void f2()
+        {
+            f1();
+        }
+        static void f1()
+        {
+            int a = 5;
+            int b = 1;
+            Console.Write(a/b);
+        }
+    }
+}
